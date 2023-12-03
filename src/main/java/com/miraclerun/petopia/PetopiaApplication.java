d9ac8ff8421933d82ed9ddff5fc35f2d1bf6d2ae
onlyplsson <onlyplsson@gmail.com>
2023-12-03 21:11:56 +0900

    first commit

---
+++ src/main/java/com/miraclerun/petopia/PetopiaApplication.java
@@ -0,0 +1,13 @@
+package com.miraclerun.petopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetopiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetopiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miraclerun/petopia/PetopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miraclerun.petopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetopiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
