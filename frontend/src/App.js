3d19c7973571caf81b94477303d8dc6c2002b70c
Star <gksquf9720@naver.comm>
2023-12-03 21:47:31 +0900

    frontend 래익트 초기 설정

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          petopla에 오신 것을 환영합니다.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
