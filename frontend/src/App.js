786f254aceb2fe24db1c9ec6477ede7b8801c017
Star <gksquf9720@naver.comm>
2023-12-05 21:39:06 +0900

    ant design css 프레임워크 테스트

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          petopia에 오신 것을 환영합니다.
-          애완동물의 사진을 공유하고 자랑해보세요.
-        </p>
-      </header>
-    </div>
-  );
-}
+// import './App.css';
+// import { DatePicker } from 'antd';
+import {Button} from "antd";
+const App = () => (
+  <div className='btn'>
+    <Button type="primary">Primary Button</Button>
+    <Button>Default Button</Button>
+    <Button type="dashed">Dashed Button</Button>
+    <Button type="text">Text Button</Button>
+    <Button type="link">Link Button</Button>
+  </div>
+);
 
 export default App;
--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,4 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-hi
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
