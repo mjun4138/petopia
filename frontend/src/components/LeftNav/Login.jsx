b6390155e1497f49c5a59ae97818945989cdf293
onlyplsson <onlyplsson@gmail.com>
2023-12-31 03:30:20 +0900

    회원가입 API 구현

--- frontend/src/components/LeftNav/Login.jsx
+++ frontend/src/components/LeftNav/Login.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import {Link} from "react-router-dom";
+import axios from "axios";
 
 const Login = () => {
     const [showSignUpClick, setSignUpClick] = useState(false)
@@ -27,15 +28,68 @@ const Login = () => {
     }
 
     const SignUpForm = () => {
+        const [createMemberRequest, setRequest] = useState({
+            email: '',
+            name: '',
+            account: '',
+            password: ''
+        })
+
+        const [valid, setValid] = useState({
+            email: '',
+            name: '',
+            account: '',
+            password: ''
+        })
+
+        const handleChange = (e) => {
+            setRequest({
+                ...createMemberRequest,
+                [e.target.name]: e.target.value
+            })
+        }
+
+        const createMember = async () => {
+            try {
+                await axios.post("/api/members", createMemberRequest)
+                setSignUpClick(false)
+                alert("가입이 완료되었습니다.")
+            } catch (error) {
+                setValid({
+                    email: error.response.data.validation.email,
+                    name: error.response.data.validation.name,
+                    account: error.response.data.validation.account,
+                    password: error.response.data.validation.password
+                })
+                console.log(error.response.data);
+                console.log(error.response.data.validation.email)
+                console.log({valid})
+            }
+
+        }
+
         return (
             <div className='signup-form'>
                 <div className='signup'>
                     <h1>petopia</h1>
-                    <input type="email" id='email' placeholder='이메일'/>
-                    <input type="text" id='name' placeholder='성명'/>
-                    <input type="text" id='id' placeholder='아이디'/>
-                    <input type="password" id='pw' placeholder='비밀번호'/>
-                    <button onClick={() => setSignUpClick(false)}>가입</button>
+                    <input type="email" name="email" value={createMemberRequest.email} placeholder='이메일' onChange={handleChange}/>
+                    <input type="text" name="name" value={createMemberRequest.name} placeholder='성명' onChange={handleChange}/>
+                    <input type="text" name="account" value={createMemberRequest.account} placeholder='아이디' onChange={handleChange}/>
+                    <input type="password" name="password" value={createMemberRequest.password} placeholder='비밀번호' onChange={handleChange}/>
+                    <button onClick={createMember}>가입</button>
+                    <span className="signup-error">
+                        {
+                            valid.email ?
+                                <>{valid.email}</>
+                                : valid.name ?
+                                <>{valid.name}</>
+                                : valid.account ?
+                                    <>{valid.account}</>
+                                    : (
+                                        <>{valid.password}</>
+                                        )
+                        }
+                    </span>
                 </div>
 
                 <div className='register'>
