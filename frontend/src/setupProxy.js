a45322c58f8e539ef689ad5a92c7dad2c7e0a438
onlyplsson <onlyplsson@gmail.com>
2023-12-31 00:44:28 +0900

    setupProxy.js path 변경

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware');
 
 module.exports = (app) => {
     app.use(
-        '/',
+        '/api',
         createProxyMiddleware({
             target: 'http://localhost:8080',
             changeOrigin: true,
